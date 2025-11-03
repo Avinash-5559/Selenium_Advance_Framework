d929bb4c8ab741274f4ca8863c15ff9dad3ddf7e
Avinash Sinha <sinha.avinash906@gmail.com>
2025-11-03 20:22:39 +0530

    Selenium Advance Framework

---
+++ src/main/java/com/avinashsinha/pages/pageFactory/katalonCURA/BookingPage_PF.java
@@ -0,0 +1,73 @@
+package com.avinashsinha.pages.pageFactory.katalonCURA;
+
+import com.avinashsinha.base.CommonToAllPage;
+import com.avinashsinha.utils.PropertiesReader;
+import com.avinashsinha.utils.WaitHelpers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//This is Page Class
+public class BookingPage_PF extends CommonToAllPage {
+
+    WebDriver driver;
+
+    public BookingPage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    //Step 1 : These are Page Locators
+    @FindBy(xpath = "//input[@id='chk_hospotal_readmission']")
+    private WebElement checkBox;
+
+    @FindBy(xpath = "//input[@id='radio_program_medicaid']")
+    private WebElement radioButton;
+
+    @FindBy(xpath = "//input[@id='txt_visit_date']")
+    private WebElement calendar;
+
+    @FindBy(xpath = "//div[contains(@class,'datepicker-dropdown')]")
+    private WebElement openCalendar;
+
+    @FindBy(xpath = "(//th[@class='datepicker-switch'])[1]")
+    private WebElement currentMonth;
+
+    @FindBy(xpath = "(//th[@class='next'])[1]")
+    private WebElement next;
+
+    @FindBy(xpath = "(//td[@class='day'])[26]")
+    private WebElement day;
+
+    @FindBy(xpath = "//textarea[@id='txt_comment']")
+    private WebElement comment;
+
+    @FindBy(xpath = "//button[@id='btn-book-appointment']")
+    private WebElement bookAppointment;
+
+    //Step 2 : These are Page Actions
+    public void fillAllDetailsOnBookingPage() {
+
+        WaitHelpers.urlContains(driver, PropertiesReader.readKey("katalon_bookingURL"));
+
+        clickElement(checkBox);
+        clickElement(radioButton);
+        clickElement(calendar);
+
+        WaitHelpers.visibilityOfElement(openCalendar);
+
+        while (!getText(currentMonth).equals("July 2026")) {
+
+            clickElement(next);
+            getText(currentMonth);
+
+        }
+
+        clickElement(day);
+
+        enterInput(comment, PropertiesReader.readKey("katalon_comment"));
+        clickElement(bookAppointment);
+
+    }
+}
---
+++ src/main/java/com/avinashsinha/pages/pageObjectModel/katalonCURA/BookingPage.java
@@ -0,0 +1,54 @@
+package com.avinashsinha.pages.pageObjectModel.katalonCURA;
+
+import com.avinashsinha.base.CommonToAllPage;
+import com.avinashsinha.utils.PropertiesReader;
+import com.avinashsinha.utils.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+//This is Page Class
+public class BookingPage extends CommonToAllPage {
+
+    WebDriver driver;
+
+    public BookingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Step 1 : These are Page Locators
+    private By checkBox = By.xpath("//input[@id='chk_hospotal_readmission']");
+    private By radioButton = By.xpath("//input[@id='radio_program_medicaid']");
+    private By calendar = By.xpath("//input[@id='txt_visit_date']");
+    private By openCalendar = By.xpath("//div[contains(@class,'datepicker-dropdown')]");
+    private By currentMonth = By.xpath("(//th[@class='datepicker-switch'])[1]");
+    private By next = By.xpath("(//th[@class='next'])[1]");
+    private By day = By.xpath("(//td[@class='day'])[26]");
+    private By comment = By.xpath("//textarea[@id='txt_comment']");
+    private By bookAppointment = By.xpath("//button[@id='btn-book-appointment']");
+
+    //Step 2 : These are Page Actions
+    public void fillAllDetailsOnBookingPage() {
+
+        WaitHelpers.urlContains(driver, PropertiesReader.readKey("katalon_bookingURL"));
+
+        clickElement(checkBox);
+        clickElement(radioButton);
+        clickElement(calendar);
+
+        WaitHelpers.presenceOfElement(driver, openCalendar);
+
+        while (!getText(currentMonth).equals("July 2026")) {
+
+            clickElement(next);
+            getText(currentMonth);
+
+        }
+
+        clickElement(day);
+
+        enterInput(comment, PropertiesReader.readKey("katalon_comment"));
+        clickElement(bookAppointment);
+
+    }
+
+}
