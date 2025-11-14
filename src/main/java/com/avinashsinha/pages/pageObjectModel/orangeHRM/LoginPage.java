3a1c759c1df421983993b82eb5824da83dbcc18d
Avinash Sinha <sinha.avinash906@gmail.com>
2025-11-14 16:16:10 +0530

    Selenium Advance Framework

---
+++ src/main/java/com/avinashsinha/pages/pageObjectModel/orangeHRM/LoginPage.java
@@ -0,0 +1,38 @@
+package com.avinashsinha.pages.pageObjectModel.orangeHRM;
+
+import com.avinashsinha.base.CommonToAllPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.avinashsinha.utils.WaitHelpers.waitJVM;
+
+//This is Page Class
+public class LoginPage extends CommonToAllPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Step 1 : These are Page Locators
+    private By username = By.xpath("//input[@placeholder='Username']");
+    private By password = By.xpath("//input[@placeholder='Password']");
+    private By submitButton = By.xpath("//button[normalize-space()='Login']");
+
+    //Step 2 : These are Page Actions
+    public void loginToOrangeHRMCreds(String usr, String pwd) {
+
+        openOrangeHRMUrl();
+
+        waitJVM(5000);
+
+        enterInput(username, usr);
+        enterInput(password, pwd);
+        clickElement(submitButton);
+
+        waitJVM(5000);
+
+    }
+
+}
