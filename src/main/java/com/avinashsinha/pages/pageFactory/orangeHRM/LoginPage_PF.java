37cf4923eccea62824afeb0ee5b495124087f177
Avinash Sinha <sinha.avinash906@gmail.com>
2025-11-15 16:34:52 +0530

    Selenium Advance Framework

---
+++ src/main/java/com/avinashsinha/pages/pageFactory/orangeHRM/LoginPage_PF.java
@@ -0,0 +1,45 @@
+package com.avinashsinha.pages.pageFactory.orangeHRM;
+
+import com.avinashsinha.base.CommonToAllPage;
+import com.avinashsinha.utils.PropertiesReader;
+import com.avinashsinha.utils.WaitHelpers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//This is Page Class
+public class LoginPage_PF extends CommonToAllPage {
+
+    WebDriver driver;
+
+    public LoginPage_PF(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    //Step 1 : These are Page Locators
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    private WebElement username;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[normalize-space()='Login']")
+    private WebElement submitButton;
+
+    //Step 2 : These are Page Actions
+    public void loginToOrangeHRMCreds() {
+
+        openOrangeHRMUrl();
+
+        WaitHelpers.waitJVM(5000);
+
+        enterInput(username, PropertiesReader.readKey("ohr_username"));
+        enterInput(password, PropertiesReader.readKey("ohr_password"));
+        clickElement(submitButton);
+
+        WaitHelpers.waitJVM(5000);
+    }
+
+}
