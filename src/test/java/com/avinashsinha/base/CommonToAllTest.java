ade6855ef06e48c6a9eb4652fa58ac6e30f4ad89
Avinash Sinha <sinha.avinash906@gmail.com>
2025-12-27 16:10:24 +0530

    Selenium Advance Framework

--- src/test/java/com/avinashsinha/base/CommonToAllTest.java
+++ src/test/java/com/avinashsinha/base/CommonToAllTest.java
@@ -4,7 +4,6 @@ import com.avinashsinha.driver.DriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-
 public class CommonToAllTest {
 
     @BeforeMethod
