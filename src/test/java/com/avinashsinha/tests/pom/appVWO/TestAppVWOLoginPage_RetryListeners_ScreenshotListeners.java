f63ad04e1ed30db9c7a654472f8ef35f00548da9
Avinash Sinha <sinha.avinash906@gmail.com>
2026-04-20 00:24:13 +0530

    Selenium Advance Framework

--- src/test/java/com/avinashsinha/tests/pom/appVWO/TestAppVWOLoginPage_RetryListeners_ScreenshotListeners.java
+++ src/test/java/com/avinashsinha/tests/pom/appVWO/TestAppVWOLoginPage_RetryListeners_ScreenshotListeners.java
@@ -17,7 +17,7 @@ import static com.avinashsinha.driver.DriverManager.getDriver;
 @Test(retryAnalyzer = RetryAnalyzer.class)
 public class TestAppVWOLoginPage_RetryListeners_ScreenshotListeners extends CommonToAllTest {
 
-    private static final Logger logger= LogManager.getLogger(TestAppVWOLoginPage_RetryListeners_ScreenshotListeners.class);
+    private static final Logger LOGGER = LogManager.getLogger(TestAppVWOLoginPage_RetryListeners_ScreenshotListeners.class);
 
     @Test
     @Description("TC#1: Verify that with Invalid Username and Password, get error message on the Login Page")
@@ -26,20 +26,20 @@ public class TestAppVWOLoginPage_RetryListeners_ScreenshotListeners extends Comm
 
         getDriver().get("https://app.vwo.com");
 
-        logger.info("Starting the Testcases Page Object Model");
+        LOGGER.info("Starting the Testcases Page Object Model");
 
         Assert.assertTrue(false);
 
     }
 
     @Test
-    @Description("TC#1: Verify that with Invalid Username and Password, get error message on the Login Page")
+    @Description("TC#2: Verify that with Invalid Username and Password, get error message on the Login Page")
     @Owner("Avinash Sinha")
     public void testCase_Pass() {
 
         getDriver().get("https://app.vwo.com");
 
-        logger.info("Starting the Testcases Page Object Model");
+        LOGGER.info("Starting the Testcases Page Object Model");
 
         Assert.assertTrue(true);
 
